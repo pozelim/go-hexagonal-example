347d345ee0cf6a2a9a2c6f01e988b80982879724
Pedro Ozelim <pedrohozelim@gmail.com>
2022-07-23 16:13:44 -0300

    Fix tests

--- internal/app/service/estate_test.go
+++ internal/app/service/estate_test.go
@@ -44,7 +44,7 @@ func TestEstateService_Create(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := &Estate{
-				estateRepository: tt.fields.estateRepository,
+				estateStorage: tt.fields.estateRepository,
 			}
 			got, err := e.Create(tt.args.estate)
 			if (err != nil) != tt.wantErr {
